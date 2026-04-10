6036ee2be5672716106a2cd53d09ca8116f3c12c
priyanshu11223344 <priyanshupanchal2105@gmail.com>
2026-04-10 20:45:53 +0530

    done

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,5 +6,16 @@ const API = axios.create({
   withCredentials:true,
   
 });
+// 🔥 ADD THIS BLOCK
+API.interceptors.request.use(
+  (req) => {
+    console.log("🚀 API CALL:", req.method.toUpperCase(), req.baseURL + req.url);
+    console.trace(); // 🔥 THIS WILL SHOW EXACT SOURCE
+    return req;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default API;
